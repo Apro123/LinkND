00f9c4bb2076951a5b2936274f98486a60a2d6df
Apro123 <akapoor3@ucmerced.edu>
2020-12-10 11:27:57 -0800

    modified file path and added comments

--- main.py
+++ main.py
@@ -9,28 +9,14 @@ import time
 p = subprocess.Popen(["motelist | cut -d ' ' -f 4 | grep -v -e '^$'"], stdout=subprocess.PIPE, shell=True)
 motes = p.stdout.read().split()
 
-#get the time needed to run the session
-try:
-	sessionTime = int(sys.argv[1])
-except:
-	print("Please provide a valid number of seconds you want the program to run")
-	exit()
-
-#get the session number
-#check next session number from file structure: "Session_X_Node_XX.txt"
-try:
-	p = subprocess.Popen(["ls /home/pi/Desktop/EECS262/mountedFileSystem/MoteOutput/Session* | sort -n -t _ -k 2"], stdout=subprocess.PIPE, shell=True)
-	nextSession = int(p.stdout.read().split()[-1].split('_')[1]) + 1
-except:
-	nextSession = 0
-print("Session is now number: " + str(nextSession))
-
 time.sleep(3)
 #build and run the motes
 os.system("./moteBuildandRun.sh")
 
 for i in range(len(motes)):
 	nodeid = i+1
-	subprocess.Popen(['python', './readserial.py', str(sessionTime), str(nextSession), str(nodeid), str(motes[i])])
+	subprocess.Popen(['python', './readserial.py', str(nodeid), str(motes[i])])
 
 print("Started subprocesses")
+
+#pkill -f "readserial"
--- readserial.py
+++ readserial.py
@@ -4,24 +4,25 @@ import serial
 import sys
 import time
 
-timeToRun = int(sys.argv[1])
-sessionNum = int(sys.argv[2])
-nodeid = int(sys.argv[3])
-moteport = sys.argv[4]
+nodeid = int(sys.argv[1])
+moteport = sys.argv[2]
 
 moteSerial = serial.Serial(port=moteport,baudrate=115200,timeout=1)
 
-fileToWrite = open("/home/pi/Desktop/EECS262/mountedFileSystem/MoteOutput/Session_" + str(sessionNum) + "_Node_" + str(nodeid) + ".txt", "wb")
+fileToWrite = open("/home/pi/Desktop/EECS262/mountedFileSystem/MoteOutput/Node_" + str(nodeid) + ".txt", "wb")
 
-t_end = time.time() + timeToRun
-while(time.time() <= t_end):
+while(True):
 	data = moteSerial.read(40).decode('ascii', 'ignore')
 	if(data != ""):
 		try:
-			data = data.split("-")[1]
-			fileData = str(time.time()) + "--" + data + "--\n"
-			#print(fileData)
-			fileToWrite.write(fileData)
+			data = data.split("-")[1].strip().split(",")
+			if(len(data) == 4):
+				lineData = [int(x) for x in data]
+				
+				fileData = str(time.time()) + " "
+				for pt in lineData:
+					fileData += str(pt) + ","
+				fileToWrite.write(fileData[:-1] + "\n")
 			#files[i].write(fileData)
 		except:
 			#print(data.split("-")[0])
